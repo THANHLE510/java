1f65fbebe3f902c4704c2035490c6d6db62447d0
THANHLE510 <111995033+THANHLE510@users.noreply.github.com>
2023-03-06 22:49:18 +0700

    Add files via upload

---
+++ ArrayListObjectSortExample.java
@@ -0,0 +1,14 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListObjectSortExample {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Sachin",47));
+        people.add(new Person("Chris",34));
+        people.add(new Person("Rajeev",25));
+        people.add(new Person("David",31));
+    }
+}
