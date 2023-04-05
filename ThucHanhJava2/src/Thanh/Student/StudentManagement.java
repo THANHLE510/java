6485ff215e3e773f4b2f502cf12c31b4ddc83334
THANHLE510 <111995033+THANHLE510@users.noreply.github.com>
2023-04-05 10:36:09 +0700

    Add files via upload

---
+++ ThucHanhJava2/src/Thanh/Student/StudentManagement.java
@@ -0,0 +1,140 @@
+package JAVA_2.SQL.EXAM.ThucHanhJava2.src.Thanh.Student;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentManagement {
+    private ArrayList<Student> studentList = new ArrayList<>();
+    public void run() {
+        while (true) {
+            displayMenu();
+            int option = getOption();
+            switch (option) {
+                case 1:
+                    addStudent();
+                    break;
+                case 2:
+                    displayStudents();
+                    break;
+                case 3:
+                    saveStudents();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid option!");
+            }
+        }
+    }
+
+    public void displayMenu() {
+        System.out.println("\n----------File Student----------");
+        System.out.println("1. Add student records");
+        System.out.println("2. Display student records");
+        System.out.println("3. Save");
+        System.out.println("4. Exit");
+    }
+
+    public int getOption() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter option: ");
+        return scanner.nextInt();
+    }
+
+    public void addStudent() {
+        System.out.println("\n-----Add student records-----");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter student ID: ");
+        String studentID = scanner.next();
+
+        System.out.print("Enter name: ");
+        String name = scanner.next();
+
+        System.out.print("Enter address: ");
+        String address = scanner.next();
+
+        System.out.print("Enter phone: ");
+        String phone = scanner.next();
+
+        Student student = new Student(studentID, name, address, phone);
+        studentList.add(student);
+        System.out.println("Đã thêm sinh viên vào danh sách chờ");
+    }
+
+    public void displayStudents() {
+        System.out.println("\n-----Display student records-----");
+        System.out.println("Student ID\t\tName\tAddress\tPhone");
+        for (Student student : studentList) {
+            System.out.println(student.getStudentID() + "\t\t" + student.getName() + "\t" + student.getAddress() + "\t\t" + student.getPhone());
+        }
+    }
+
+    public void saveStudents() {
+        for (Student student : studentList) {
+            try(
+                    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/file_student", "root", "");
+                    ){
+                PreparedStatement pstmt = conn.prepareStatement("insert into student values (?,?,?,?)");
+
+                pstmt.setString(1,student.getStudentID());
+                pstmt.setString(2,student.getName());
+                pstmt.setString(3,student.getAddress());
+                pstmt.setString(4,student.getPhone());
+
+                int check = pstmt.executeUpdate();
+                if(check >= 1 ){
+                    System.out.println("Update roi");
+                } else {
+                    System.out.println("Update roi");
+                }
+            }catch (SQLException ex){
+                throw new RuntimeException(ex);
+            }
+            int i = -1;
+            i++;
+            studentList.remove(i);
+            break;
+        }
+    }
+
+    public static void main(String[] args) {
+        StudentManagement studentManagement = new StudentManagement();
+        studentManagement.run();
+    }
+}
+
+class Student {
+    private String studentID;
+    private String name;
+    private String address;
+    private String phone;
+
+    public Student(String studentID, String name, String address, String phone) {
+        this.studentID = studentID;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    public String toString(){
+        return "Student ID: " + studentID + ", Name: " + name + ", Address: " + address + ", Phone: " + phone;
+    }
+
+}
---
+++ ThucHanhJava2/student.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th4 05, 2023 lúc 05:18 AM
+-- Phiên bản máy phục vụ: 10.4.27-MariaDB
+-- Phiên bản PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `file_student`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `student`
+--
+
+CREATE TABLE `student` (
+  `StudentID` varchar(20) NOT NULL,
+  `Name` varchar(100) DEFAULT NULL,
+  `Address` varchar(100) DEFAULT NULL,
+  `Phone` int(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `student`
+--
+
+INSERT INTO `student` (`StudentID`, `Name`, `Address`, `Phone`) VALUES
+('1', 'edan', 'hanoi', 365741416);
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`StudentID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
